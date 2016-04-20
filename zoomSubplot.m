0ffce6684a8f2de0cae2f9ef5ed26c8caaf22a27
Ondiz <ozarraga@mondragon.edu>
2016-04-20 15:02:57 +0200

    Add licensing

--- zoomSubplot.m
+++ zoomSubplot.m
@@ -9,6 +9,21 @@ function zoomSubplot(figNum,r,c,axis,lim1,lim2)
 % lim1: limit to axis if single
 % lim2: limits to second axis (x and then y)
 
+% Copyright (C) 2016  Ondiz Zarraga
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 figure(figNum)
 
 num = length(findall(gcf,'type','axes')); % detect number of subplots in plot
